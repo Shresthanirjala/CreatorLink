cd16c51eefa564c975b0c545f3938957787a1e1f
Abishkardhenga <copywriterayan47@gmail.com>
2025-05-14 01:56:42 +0545

    bug fixed plus additionla code

--- programs/creator-link/src/lib.rs
+++ programs/creator-link/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token_interface::{self, Mint, TokenAccount, TokenInterface};
 
-declare_id!("5rpuD1rYDBoQJ7jzw9FdFEe7QFBipaMvT8k1T1p1D1ft ");
+declare_id!("5xbQwuduFoaEQUKG7waf2oGiMvrPfZY5DFdLLMwtjtpb");
 
 #[program]
 pub mod creator_link {
@@ -30,31 +30,69 @@ pub mod creator_link {
 
     pub fn buy_t(ctx: Context<BuyToken>, token_to_bought: u64) -> Result<()> {
         let statedata = &mut ctx.accounts.creator_state;
-    
+        let price = statedata.base_price + (statedata.slope * statedata.tokens_sold);
+
+        require!(
+            **ctx.accounts.buyer.to_account_info().lamports.borrow() >= price,
+            CustomError::InsufficientFunds
+        );
+
         let cpi_accounts = token_interface::TransferChecked {
             mint: ctx.accounts.mint.to_account_info(),
             from: ctx.accounts.vault_token_account.to_account_info(),
             to: ctx.accounts.buyer_token_account.to_account_info(),
             authority: ctx.accounts.vault_authority.to_account_info(),
         };
-    
+
         let cpi_program = ctx.accounts.token_program.to_account_info();
         let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
-    
+
         token_interface::transfer_checked(
             cpi_context,
             token_to_bought,
             ctx.accounts.mint.decimals
         )?;
-    
-        statedata.tokens_sold += token_to_bought;
+
+        **ctx.accounts.buyer.try_borrow_mut_lamports()? -= price;
+        **ctx.accounts.creator.try_borrow_mut_lamports()? += price;
+
+        update_token_counter(statedata, token_to_bought);
+
+        emit!(TokenPurchased {
+            buyer: ctx.accounts.buyer.key(),
+            amount: token_to_bought,
+            price_paid: price,
+        });
+
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<WithdrawFunds>, amount: u64) -> Result<()> {
+        require_keys_eq!(
+            ctx.accounts.creator.key(),
+            ctx.accounts.creator_state.creator,
+            CustomError::Unauthorized
+        );
+
+        require!(
+            **ctx.accounts.vault_account.try_borrow_lamports()? >= amount,
+            CustomError::InsufficientFunds
+        );
+
+        **ctx.accounts.vault_account.try_borrow_mut_lamports()? -= amount;
+        **ctx.accounts.destination.try_borrow_mut_lamports()? += amount;
+
         Ok(())
     }
 }
 
+fn update_token_counter(state: &mut CreatorState, amount: u64) {
+    state.tokens_sold = state.tokens_sold.checked_add(amount).unwrap();
+}
+
 #[derive(Accounts)]
 pub struct InitializeCreator<'info> {
-   #[account(init, payer = creator, space = 8 + 32 * 2 + 8 * 5 + 4 + 220)]
+    #[account(init, payer = creator, space = 8 + 32 * 2 + 8 * 4 + 4 + 220)]
     pub creator_state: Account<'info, CreatorState>,
     #[account(mut)]
     pub creator: Signer<'info>,
@@ -98,7 +136,7 @@ pub struct BuyToken<'info> {
         token::token_program = token_program
     )]
     pub vault_token_account: InterfaceAccount<'info, TokenAccount>,
-    
+
     #[account(
         mut,
         token::token_program = token_program
@@ -113,11 +151,28 @@ pub struct BuyToken<'info> {
     pub vault_authority: UncheckedAccount<'info>,
 
     pub token_program: Interface<'info, TokenInterface>,
-    
+
     #[account(
         mint::token_program = token_program
     )]
     pub mint: InterfaceAccount<'info, Mint>,
+}
+
+
+#[derive(Accounts)]
+pub struct WithdrawFunds<'info> {
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    #[account(mut)]
+        /// CHECK: We assume that this is the creator's vault account and it is safe to access directly.
+
+    pub vault_account: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub destination: AccountInfo<'info>,
+
+    pub creator_state: Account<'info, CreatorState>,
 }
 
 #[account]
@@ -135,4 +190,13 @@ pub struct CreatorState {
 pub enum CustomError {
     #[msg("Buyer has insufficient SOL to purchase token.")]
     InsufficientFunds,
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+}
+
+#[event]
+pub struct TokenPurchased {
+    pub buyer: Pubkey,
+    pub amount: u64,
+    pub price_paid: u64,
 }
